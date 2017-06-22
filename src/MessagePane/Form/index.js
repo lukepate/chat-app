673f14a4a974a9260ba8b6a267c2512264f458ea
luke pate <lukewpate@gmail.com>
2017-06-21 20:25:08 -0400

    add buttons

--- src/MessagePane/Form/index.js
+++ src/MessagePane/Form/index.js
@@ -1,26 +1,40 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const Form = () => (
-  <div className="MessagePane-Form">
-    <div className="MessagePane-Form-container">
-      <p>
-        <input
-          className="name"
-          type="text"
-          placeholder="some_login_here_places"
-        />
-      </p>
-      <p>
-        <textarea
-        className="message"
-        placeholder="Your Message"
-      />
-      </p>
-      <p>
-        <button className="send">Send</button>
-      </p>
-    </div>
-  </div>
-);
+class Form extends Component {
+  constructor(){
+    super();
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onClick() {
+
+  }
+
+  render() {
+    return (
+      <div className="MessagePane-Form">
+        <div className="MessagePane-Form-container">
+          <p>
+            <input
+              ref={(node) => {this.name_input = node}}
+              className="name"
+              type="text"
+              placeholder="some_login_here_places"
+            />
+          </p>
+          <p>
+            <textarea
+            className="message"
+            placeholder="Your Message"
+          />
+          </p>
+          <p>
+            <button className="send" onClick={this.OnClick}>Send</button>
+          </p>
+        </div>
+      </div>
+    );
+  }
+};
 
 export default Form;
